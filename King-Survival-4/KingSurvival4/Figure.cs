457af84a99a543d81d2104ca52728c30270c80d2
izabelk <izi_girl@abv.bg>
2014-07-14 15:11:30 +0300

    added a Figure class

---
+++ King-Survival-4/KingSurvival4/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    public class Figure
+    {
+        private char symbol;
+
+        public Figure(char symbol)
+        {
+            this.Symbol = symbol;
+        }
+
+        public char Symbol { get; set; }
+    }
+}
