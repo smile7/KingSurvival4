0414be5fe1e132b5199e9d45f0aea34e4e2965eb
epitsin <epitsin@yahoo.com>
2014-07-14 15:59:33 +0300

    decorator class

---
+++ King-Survival-4/KingSurvival4/Decorator.cs
@@ -0,0 +1,20 @@
+﻿namespace KingSurvival4
+{
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    internal abstract class Decorator : Figure
+    {
+        protected Decorator(Figure libraryItem)
+        {
+            this.LibraryItem = libraryItem;
+        }
+
+        protected Figure LibraryItem { get; set; }
+
+        public override void Display()
+        {
+            //skdujhf
+        }
+    }
+}
