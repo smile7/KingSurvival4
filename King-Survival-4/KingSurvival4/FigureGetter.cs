46ccd629fd267ddecaf7d14eecd69a9e7de1c710
izabelk <izi_girl@abv.bg>
2014-07-14 15:43:03 +0300

    Merge branch 'master' of https://github.com/smile7/KingSurvival4.git
    merging

---
+++ King-Survival-4/KingSurvival4/FigureGetter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    public static class FigureGetter
+    {
+        public static Figure GetFigure(char symbol){
+            switch(symbol)
+            {
+                case 'K':
+                    return new King();
+                case 'P':
+                    return new Pawn();
+                default:
+                    return new Figure();
+            }
+        }
+    }
+}
