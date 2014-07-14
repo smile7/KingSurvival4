2556a718d61ca06c09dcc1d1ce6e1290fe3292c5
epitsin <epitsin@yahoo.com>
2014-07-14 15:02:17 +0300

    created board (only instance of Board)

---
+++ King-Survival-4/KingSurvival4/Board.cs
@@ -0,0 +1,30 @@
+﻿namespace KingSurvival4
+{
+    public sealed class Board
+    {
+        private static volatile Board instance; 
+
+        private static object syncLock = new object();
+
+        private Board() { }
+
+        public static Board Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Board();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
