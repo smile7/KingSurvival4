d9e17c48e78a28c8b63f2a7effa63ff460632053
izabelk <izi_girl@abv.bg>
2014-07-15 18:59:14 +0300

    added a check method as a comment in Position class

--- King-Survival-4/KingSurvival4/Position.cs
+++ King-Survival-4/KingSurvival4/Position.cs
@@ -15,6 +15,17 @@ namespace KingSurvival4
         }
 
         //TODO: Maybe check for a valid position
+
+        //private bool IsPositionInsideBoard(int row, int col)
+        //{
+        //    if (row < 0 || row > this.board.GetLength(0) - 1 || col < 0 || col > this.board.GetLength(1) - 1)
+        //    {
+        //        return false;
+        //    }
+
+        //    return true;
+        //}
+
         public int X { get; set; }
         public int Y { get; set; }
 
