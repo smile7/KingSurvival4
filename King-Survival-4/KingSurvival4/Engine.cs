8e01e09ce45072be7f17f01f2b12ecabc651b44f
izabelk <izi_girl@abv.bg>
2014-07-15 18:53:59 +0300

    changed some constants in Engine class

--- King-Survival-4/KingSurvival4/Engine.cs
+++ King-Survival-4/KingSurvival4/Engine.cs
@@ -19,9 +19,9 @@
             const string WhiteCell = "+";
             const string BlackCell = "-";
 
-            for (int row = 0; row < 8; row++)
+            for (int row = 0; row < Board.Field.GetLength(0); row++)
             {
-                for (int col = 0; col < 8; col++)
+                for (int col = 0; col < Board.Field.GetLength(1); col++)
                 {
                     if ((row + col) % 2 == 0)
                     {
