c7f1c3a07b5ec18c140fc60db9860b0e3d1a2627
epitsin <epitsin@yahoo.com>
2014-07-15 14:17:37 +0300

    added override methods to figureparser

--- King-Survival-4/KingSurvival4/FigureParser.cs
+++ King-Survival-4/KingSurvival4/FigureParser.cs
@@ -2,5 +2,20 @@
 {
     internal class FigureParser : Parser
     {
+
+        public override void FirstLetter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SecondLetter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ThirdLetter()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
