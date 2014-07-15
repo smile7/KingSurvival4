09c5c70708183c7a4ce48eb22ec553157610833a
epitsin <epitsin@yahoo.com>
2014-07-15 15:16:25 +0300

    added class mediator and abstract mediator to check the new positions of the figures

---
+++ King-Survival-4/KingSurvival4/AbstractMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    internal abstract class AbstractMediator
+    {
+        public abstract void AddFigure(Figure figure);
+        public abstract void ChangePosition(Figure figure, Command command);
+    }
+}
---
+++ King-Survival-4/KingSurvival4/Mediator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    internal class Mediator : AbstractMediator
+    {
+        List<Figure> listOfFigures = new List<Figure>();
+        public override void AddFigure(Figure figure)
+        {
+            this.listOfFigures.Add(figure);
+        }
+
+        public override void ChangePosition(Figure figure, Command comand)
+        {
+            //TO DO: check if new position is not available or is outside the board
+        }
+    }
+}
