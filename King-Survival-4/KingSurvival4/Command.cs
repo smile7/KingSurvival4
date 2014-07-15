114d06ec1828f4e44619f39adf608611e4309635
izabelk <izi_girl@abv.bg>
2014-07-15 14:04:40 +0300

    added a class Command

---
+++ King-Survival-4/KingSurvival4/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    public class Command
+    {
+        public string Input { get; set; }
+    }
+}
