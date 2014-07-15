9bab2be59b0afb4012cc5db175365eaa4db18a0c
izabelk <izi_girl@abv.bg>
2014-07-15 14:26:10 +0300

    added a KingSurvival4Demo class

---
+++ King-Survival-4/KingSurvival4/KingSurvival4Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    public class KingSurvival4Demo
+    {
+        Engine engine = new Engine();
+        
+    }
+}
