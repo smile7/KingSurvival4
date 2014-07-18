b294c5b1e3e6500ebc41b25c6dae1393e7f6175d
epitsin <epitsin@yahoo.com>
2014-07-18 12:45:36 +0300

    added interfaces IReader and IRenderer and class KingSurvival

---
+++ King-Survival-4/KingSurvival4/IRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival4
+{
+    public interface IRenderer
+    {
+        void Render();
+        void Clear();
+    }
+}
---
+++ King-Survival-4/KingSurvival4/KingSurvival.cs
@@ -0,0 +1,14 @@
+﻿namespace KingSurvival4
+{
+    public class KingSurvival
+    {
+        protected IRenderer Renderer {get; private set;}
+        protected IReader Reader {get; private set;}
+
+        public KingSurvival(IReader reader, IRenderer renderer)
+        {
+            this.Reader = reader;
+            this.Renderer = renderer;
+        }
+    }
+}
