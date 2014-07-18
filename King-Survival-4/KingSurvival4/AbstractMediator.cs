0fd79eae001b39710f78f8203144f63018eb7369
epitsin <epitsin@yahoo.com>
2014-07-18 12:27:00 +0300

    deleted abstract mediator

--- King-Survival-4/KingSurvival4/AbstractMediator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KingSurvival4
-{
-    internal abstract class AbstractMediator
-    {
-        public abstract void AddFigure(Figure figure);
-
-        public abstract void ChangePosition(Figure figure, Command command);
-    }
-}
