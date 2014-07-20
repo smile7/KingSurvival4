5a115dcbf8304d267197c592ff7d097a4ceda50f
Mihail Bonev <mnbonev@gmail.com>
2014-07-20 19:09:37 +0300

    Zero Unit test - King won in 7 turns
    Check the result when playing based on commands given in the task

---
+++ UnitTestsKingSurvival/KingSurvivalUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingSurvival4;
+using System.IO;
+
+namespace UnitTestsKingSurvival
+{
+    [TestClass]
+    public class KingSurvivalUnitTest
+    {
+        [TestMethod]
+        public void KingWinsIn7Moves()
+        {
+            StringReader input = new StringReader("kul\nadl\nadr\nkur\nadl\nkur\nbdr\nkul\nbdl\nkur\ncdl\nkur\nddr\nkur");
+            Console.SetIn(input);
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+            KingSurvivalEngine testEngine = new KingSurvivalConsoleEngine(new ConsoleReader(), new ConsoleRenderer());
+            testEngine.Start();
+            string outputStr = output.ToString();
+            outputStr = outputStr.Substring(0, outputStr.Length - 2);
+            int index = outputStr.LastIndexOf("\n");
+            string toCompare = outputStr.Substring(index+1);
+            Assert.AreEqual("King won in 7 turns", toCompare, "When playing based on the zero test scenario, King didn't win.");
+        }
+    }
+}
