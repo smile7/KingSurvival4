b82520fc0d7e12215695fbb2be11bbf36bf92fc6
epitsin <epitsin@yahoo.com>
2014-07-21 17:58:55 +0300

    renamed unit testing projects

--- King-Survival-4/UnitTesting/BoardTests.cs
+++ King-Survival-4/UnitTesting/BoardTests.cs
@@ -35,7 +35,7 @@ namespace UnitTesting
                     }
                 }
 
-                if(!areEqual)
+                if (!areEqual)
                 {
                     break;
                 }
@@ -45,7 +45,7 @@ namespace UnitTesting
         }
 
         [TestMethod]
-        public void BoardInstanceFieldFillingWithFiguresTest()
+        public void BoardInstanceNotifyWithFigureTest()
         {
             Board board = Board.Instance;
             board.Notify(new Pawn(new Position(0, 0), 'A'));
@@ -63,6 +63,48 @@ namespace UnitTesting
                                { "-", "+", "-", "+", "-", "+", "-", "+" }, 
                                { "+", "-", "+", "-", "+", "-", "+", "-" }, 
                                { "-", "+", "-", "K", "-", "+", "-", "+" } 
+                               };
+
+            bool areEqual = true;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] != Board.Field[i, j])
+                    {
+                        areEqual = false;
+                        break;
+                    }
+                }
+
+                if (!areEqual)
+                {
+                    break;
+                }
+            }
+
+            Assert.IsTrue(areEqual);
+        }
+
+        [TestMethod]
+        public void BoardInstanceNotifyWithNewPositionOfFigureTest()
+        {
+            Board board = Board.Instance;
+            board.Notify(new Pawn(new Position(0, 0), 'A'));
+            board.Notify(new Pawn(new Position(0, 2), 'B'));
+            board.Notify(new Pawn(new Position(0, 4), 'C'));
+            board.Notify(new Pawn(new Position(0, 6), 'D'));
+            board.Notify(new King(new Position(6, 2)), new Position(7, 3));
+
+            string[,] matrix = { 
+                               { "A", "-", "B", "-", "C", "-", "D", "-" }, 
+                               { "-", "+", "-", "+", "-", "+", "-", "+" }, 
+                               { "+", "-", "+", "-", "+", "-", "+", "-" }, 
+                               { "-", "+", "-", "+", "-", "+", "-", "+" }, 
+                               { "+", "-", "+", "-", "+", "-", "+", "-" }, 
+                               { "-", "+", "-", "+", "-", "+", "-", "+" }, 
+                               { "+", "-", "K", "-", "+", "-", "+", "-" }, 
+                               { "-", "+", "-", "+", "-", "+", "-", "+" } 
                                };
 
             bool areEqual = true;
