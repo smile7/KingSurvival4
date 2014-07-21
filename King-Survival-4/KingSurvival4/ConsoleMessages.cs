2249b7af0ad5b047217b664c23809c51a3f4157f
epitsin <epitsin@yahoo.com>
2014-07-21 12:45:15 +0300

    adde static class for messages to be printed on the console TO DO implement it

---
+++ King-Survival-4/KingSurvival4/ConsoleMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace KingSurvival4
+{
+    /// <summary>
+    /// The class containing all the messages that are printed on the board during the game
+    /// </summary>
+    public static class ConsoleMessages
+    {
+    }
+}
