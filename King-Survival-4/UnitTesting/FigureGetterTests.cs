7feb4a0a0e0ca441c6679b6f7abcec8b788a461c
izabelk <izi_girl@abv.bg>
2014-07-21 17:19:51 +0300

    added figuregetter class unit tests

---
+++ King-Survival-4/UnitTesting/FigureGetterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingSurvival4;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class FigureGetterTests
+    {
+        [TestMethod]
+        public void GetFigureMethodTesterForKing()
+        {
+            var figure = FigureGetter.GetFigure(new Position(0, 0), 'K', 'K');
+            Assert.IsTrue(figure is King);
+        }
+
+        [TestMethod]
+        public void GetFigureMethodTesterForPawn()
+        {
+            var figure = FigureGetter.GetFigure(new Position(0, 0), 'A', 'P');
+            Assert.IsTrue(figure is Pawn);
+        }
+    }
+}
--- King-Survival-4/UnitTesting/FigureTests.cs
+++ King-Survival-4/UnitTesting/FigureTests.cs
@@ -13,7 +13,8 @@ namespace UnitTesting
             Memento memento = new Memento(new Position(1, 2), 'A', 'P');
             Pawn pawn = new Pawn(new Position(1, 2), 'A');
             var figureMemento = pawn.SaveMemento();
-            
+
+            Assert.IsTrue(figureMemento is Memento);
             Assert.AreEqual(memento.Position.X, figureMemento.Position.X);
             Assert.AreEqual(memento.Position.Y, figureMemento.Position.Y);
             Assert.AreEqual(memento.Name, figureMemento.Name);
