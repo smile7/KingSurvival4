05e10c15ee50d115f337e37da5e19f04c7621f1b
izabelk <izi_girl@abv.bg>
2014-07-21 16:40:00 +0300

    added SaveMemento method unit test

---
+++ King-Survival-4/UnitTesting/FigureTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingSurvival4;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class FigureTests
+    {
+        [TestMethod]
+        public void FigureSaveMementoTester()
+        {
+            Memento memento = new Memento(new Position(1, 2), 'A', 'P');
+            Pawn pawn = new Pawn(new Position(1, 2), 'A');
+            var figureMemento = pawn.SaveMemento();
+            
+            Assert.AreEqual(memento.Position.X, figureMemento.Position.X);
+            Assert.AreEqual(memento.Position.Y, figureMemento.Position.Y);
+            Assert.AreEqual(memento.Name, figureMemento.Name);
+            Assert.AreEqual(memento.Symbol, figureMemento.Symbol);
+
+        }
+    }
+}
