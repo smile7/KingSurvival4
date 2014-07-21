1d7519f75a53f0fb0c21b99253ffcd5a919f91c0
izabelk <izi_girl@abv.bg>
2014-07-21 17:02:10 +0300

    added moveable figure moveFigure check method

---
+++ King-Survival-4/UnitTesting/MoveableFigureTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingSurvival4;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class MoveableFigureTests
+    {
+        [TestMethod]
+        public void MoveableFigureMoveMethodTester()
+        {
+            var firstFigure = new MoveableFigure(new King(new Position(0, 0)));
+            firstFigure.MoveFigure(new Position(1, 0));
+            var secondFigure = new MoveableFigure(new King(new Position(1, 0)));
+            Assert.AreEqual(firstFigure.Position.X, secondFigure.Position.X);
+            Assert.AreEqual(firstFigure.Position.Y, secondFigure.Position.Y);
+        }
+    }
+}
