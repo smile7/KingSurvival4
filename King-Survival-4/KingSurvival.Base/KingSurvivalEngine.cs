c55c3863dbe8b409da1cc251e9932f36cb440216
epitsin <epitsin@yahoo.com>
2014-07-22 19:33:05 +0300

    added comments to the engine class

--- King-Survival-4/KingSurvival.Base/KingSurvivalEngine.cs
+++ King-Survival-4/KingSurvival.Base/KingSurvivalEngine.cs
@@ -3,7 +3,8 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /// Abstract implementator for Bridge pattern
+    /// The main abstract class of the game which defines the methods and the properties that
+    /// can be used by the concrete implementators for different platforms
     /// </summary>
     public abstract class KingSurvivalEngine
     {
@@ -19,18 +20,33 @@
 
         protected IList<Figure> Figures { get; set; }
 
+        /// <summary>
+        /// The start of the game
+        /// </summary>
         public abstract void Start();
 
+        /// <summary>
+        /// Read a command from the player of the game
+        /// </summary>
+        /// <returns></returns>
         protected string GetCommand()
         {
             return this.Reader.ReadMessage();
         }
 
+        /// <summary>
+        /// Send a message to the player of the game
+        /// </summary>
+        /// <param name="message"></param>
         protected void PostMessage(string message)
         {
             this.Renderer.WriteMessage(message);
         }
 
+        /// <summary>
+        /// Prints the whole board
+        /// </summary>
+        /// <param name="board"></param>
         protected void RenderBoard(string[,] board)
         {
             this.Renderer.Clear();
