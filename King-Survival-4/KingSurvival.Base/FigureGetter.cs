6d4b7287bc0b4b82311a7da0f1cc82104ed5ac58
epitsin <epitsin@yahoo.com>
2014-07-22 19:28:15 +0300

    added comments to FigureGetter class

--- King-Survival-4/KingSurvival.Base/FigureGetter.cs
+++ King-Survival-4/KingSurvival.Base/FigureGetter.cs
@@ -1,10 +1,17 @@
 ﻿namespace KingSurvival4
 {
     /// <summary>
-    /// Simple Factory pattern - Figure 'Creator'
+    /// The Figure 'Creator' class implementing the Simple Factory pattern
     /// </summary>
     public static class FigureGetter
     {
+        /// <summary>
+        /// Creates the figures on the board
+        /// </summary>
+        /// <param name="position">The position of the figure which is going to be created</param>
+        /// <param name="name">The name of the figure which is going to be created</param>
+        /// <param name="symbol">The symbol of the figure which is going to be created</param>
+        /// <returns>Returns King or Pawn depending on the symbol</returns>
         public static Figure GetFigure(Position position, char name, char symbol)
         {
             switch (symbol)
