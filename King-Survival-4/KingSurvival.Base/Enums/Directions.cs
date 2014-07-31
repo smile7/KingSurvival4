958b89bb0d1a7a8777201173cbf149850e1e4638
epitsin <epitsin@yahoo.com>
2014-07-31 13:06:51 +0300

    added enums for direction and figureSymbols

---
+++ King-Survival-4/KingSurvival.Base/Enums/Directions.cs
@@ -0,0 +1,11 @@
+﻿namespace KingSurvival.Base.Enums
+{
+    public enum Directions
+    {
+        DownLeft = "DL",
+        DownRight = "DR",
+        UpLeft = "UL",
+        UpRight = "UR",
+
+    }
+}
---
+++ King-Survival-4/KingSurvival.Base/Enums/FigureSymbols.cs
@@ -0,0 +1,11 @@
+﻿namespace KingSurvival.Base.Enums
+{
+    public enum FigureSymbols
+    {
+        KingSymbol = 'K',
+        FirstPawnSymbol = 'A',
+        SecondPawnSymbol = 'B',
+        ThirdPawnSymbol = 'C',
+        FourthPawnSymbol = 'D',
+    }
+}
